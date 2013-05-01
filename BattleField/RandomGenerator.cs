4bac5631685f7448a0cf33c2c31d81f0f8393584
Tsvetomir Nikolov <cvetomir.nikolov@gmail.com>
2013-05-01 18:05:23 +0300

    * Initial code cleanup
    * Added new class ReandomGenerator
    * Moved all private variables to the top of the Main class
    * Added private access modifiers to the some variables

---
+++ BattleField/RandomGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace BattleField
+{
+	class RandomGenerator
+	{
+		private static readonly Random random = new Random();
+
+		public static int GetRand(int min, int max)
+		{
+			return Convert.ToInt32(random.Next(min, max));
+		}
+	}
+}
