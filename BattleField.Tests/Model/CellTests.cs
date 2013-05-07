ea2eaa84bf84e533d233f2fac9062f1a08863863
Tsvetomir Nikolov <cvetomir.nikolov@gmail.com>
2013-05-07 23:53:30 +0300

    * Added test project
    * Added unit test for Cell class

---
+++ BattleField.Tests/Model/CellTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RefactoredBattleField.Model;
+
+namespace BattleField.Tests.Model
+{
+	[TestClass]
+	public class CellTests
+	{
+		[TestMethod]
+		public void TestCellConstructorRowParameter()
+		{
+			const int ExpectedRow = 1;
+			Cell cell = new Cell(ExpectedRow, 0);
+
+			Assert.AreEqual(ExpectedRow, cell.Row);
+		}
+
+		[TestMethod]
+		public void TestCellConstructorColParameter()
+		{
+			const int ExpectedCol = 1;
+			Cell cell = new Cell(0, ExpectedCol);
+
+			Assert.AreEqual(ExpectedCol, cell.Col);
+		}
+
+		[TestMethod]
+		public void TestCellConstructorRowParameterWithNegativeValue()
+		{
+			const int ExpectedRow = -1;
+			Cell cell = new Cell(ExpectedRow, 0);
+
+			Assert.AreEqual(ExpectedRow, cell.Row);
+		}
+
+		[TestMethod]
+		public void TestCellConstructorColParameterWithNegativeValue()
+		{
+			const int ExpectedCol = -1;
+			Cell cell = new Cell(0, ExpectedCol);
+
+			Assert.AreEqual(ExpectedCol, cell.Col);
+		}
+
+		[TestMethod]
+		public void TestCellRowPropertyWithPositiveNumber()
+		{
+			const int ExpectedRow = 1;
+			Cell cell = new Cell(0, 0);
+			cell.Row = ExpectedRow;
+
+			Assert.AreEqual(ExpectedRow, cell.Row);
+		}
+
+		[TestMethod]
+		public void TestCellColPropertyPositiveNumber()
+		{
+			const int ExpectedCol = 1;
+			Cell cell = new Cell(0, 0);
+			cell.Col = ExpectedCol;
+
+			Assert.AreEqual(ExpectedCol, cell.Col);
+		}
+
+		[TestMethod]
+		public void TestCellRowPropertyWithNegativeNumber()
+		{
+			const int ExpectedRow = -1;
+			Cell cell = new Cell(0, 0);
+			cell.Row = ExpectedRow;
+
+			Assert.AreEqual(ExpectedRow, cell.Row);
+		}
+
+		[TestMethod]
+		public void TestCellColPropertyNegativeNumber()
+		{
+			const int ExpectedCol = -1;
+			Cell cell = new Cell(0, 0);
+			cell.Col = ExpectedCol;
+
+			Assert.AreEqual(ExpectedCol, cell.Col);
+		}
+	}
+}
--- BattleField/RefactoredBattleField/Program.cs
+++ BattleField/RefactoredBattleField/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RefactoredBattleField
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
 }
