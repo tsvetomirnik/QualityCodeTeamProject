77d7c93c50966406394664d07d40a912db1b0509
Veselina Baleva <bveselina@gmail.com>
2013-05-11 22:28:59 +0300

    * Add GetUserInputCell method to the class ConsoleUserInput in RefactoredBattleField project.

--- BattleField/RefactoredBattleField/View/ConsoleUserInput.cs
+++ BattleField/RefactoredBattleField/View/ConsoleUserInput.cs
@@ -16,5 +16,46 @@
 			// TODO: Implement this method
 			throw new NotImplementedException();
 		}
+
+        public Model.Cell GetUserInputCell()
+        {
+            Console.Write("Please enter coordinates: ");
+
+            string inputRowAndColumn = Console.ReadLine();
+            string[] rowAndColumnSplit = inputRowAndColumn.Split(' ');
+
+            Model.Cell cell = new Model.Cell();
+
+            bool isValidInput = true;
+            do
+            {
+                // TODO: Show as errors/warnings
+                if (rowAndColumnSplit.Length <= 0)
+                {
+                    isValidInput = false;
+                    Console.WriteLine("Invalid parameters count.");
+                    break;
+                }
+
+                int row;
+                int col;
+                if (!int.TryParse(rowAndColumnSplit[0], out row)
+                    || !int.TryParse(rowAndColumnSplit[1], out col))
+                {
+                    isValidInput = false;
+                    Console.WriteLine("Not valid parameters values.");
+                    break;
+                }
+
+                if (isValidInput)
+                {
+                    cell.Row = row;
+                    cell.Col = col;
+                }
+
+            } while (!isValidInput);
+
+            return cell;
+        }
 	}
 }
